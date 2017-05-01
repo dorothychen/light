a8e116bdb2723afa114a374b0993aae713249dae
Serena Zheng <serenaz@princeton.edu>
2017-05-01 15:21:31 -0400

    Update sketch.js

--- static/js/sketch.js
+++ static/js/sketch.js
@@ -11,7 +11,7 @@ function setup() {
 	colorMode(HSB,100);
   noStroke();
   textSize(16);
-  text("Mood", 180, 520);
+  text("Mood", 180, 420);
 	fill(0);
 	uH = random(100);
 	uS = random(100);
@@ -51,7 +51,7 @@ function makeRec() {
 
 function makeMood() {
   fill(uH, uS, uB);
-	rect(160, 530, 80, 50);
+	rect(160, 430, 80, 50);
 }
 
 function mouseDragged(){
